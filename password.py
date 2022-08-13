6fca1a40bc72507b4882316182ed1ca217632614
Sandeepbenny <sandeep@sanduchackonal@gmail.com>
2022-08-13 16:20:26 +0530

    algorithm passed

--- password.py
+++ password.py
@@ -3,3 +3,6 @@ lowercase="abcdefghijklmnopqrstuvwyxyz"
 uppercase=lowercase.upper()
 number="0123456789"
 symbols="!@#$%^&*()+=-~`.,?/';\"\;"
+use_for = uppercase+lowercase+symbols+number
+length_for_pass=10  
+password=
