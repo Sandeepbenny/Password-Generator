c859877096eaa58fc903d20f484fcd2087cfdca7
Sandeepbenny <sandeep@sanduchackonal@gmail.com>
2022-08-13 16:29:35 +0530

    Pasword Genrator Done

--- password.py
+++ password.py
@@ -4,5 +4,9 @@ uppercase=lowercase.upper()
 number="0123456789"
 symbols="!@#$%^&*()+=-~`.,?/';\"\;"
 use_for = uppercase+lowercase+symbols+number
-length_for_pass=10  
-password=
+#length_for_pass=input("value\n")
+length_for_pass=int(input("Value \n "))
+#length_for_pass1=10#
+password="".join(random.sample(use_for,length_for_pass))
+print("Generated PAss:",password)
+                 
